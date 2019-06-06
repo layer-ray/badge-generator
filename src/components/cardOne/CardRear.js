a1d48e558ab17f78893b10846d6811671d4ae94a
R-Layer <layerdev@protonmail.com>
2019-06-06 15:48:04 +0200

    add split over multiple lines fuctionality

--- src/components/cardOne/CardRear.js
+++ src/components/cardOne/CardRear.js
@@ -1,20 +1,40 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 
-import cardStyle from './card-one.module.scss';
+import style from './card-one.module.scss';
 import image from './imgs/qr_wiki_en.png';
 
-const CardRear = () => {
+const splitOverMultipleLines = (string, limit) => {
+    let i = 0;
+    let nodeArr = [];
+    while (i< string.length) {
+        i+=limit;
+        nodeArr.push(<span key={i}>{string.substr(i-limit, limit)}</span>);
+        nodeArr.push(<br key={i+1} />)
+    };
+    return nodeArr;
+}
+
+const CardRear = props => {
+    const {email, github, twitter} = props.state;
+    const {spin} = props;
     return (
-        <div className={cardStyle.test}>
-            <img src={image} alt="myImage" width="150" height="150"/>
-            <div className={cardStyle.field}>
-                <span>Test name n°1</span>
-            </div>
-            <div className={cardStyle.field}>
-                <span>Test name n°2</span>
-            </div>
-            <div className={cardStyle.field}>
-                <span>Test name n°3</span>
+        <div className={style.badge}>
+             <button onClick={spin} className={style.btn}>toggle</button>
+            <div>
+                <div className={style.field}>
+                {
+                email.length >18 
+                    ? splitOverMultipleLines(email, 17)
+                    :   <span>{email || 'Jhon_Doe@cool.com'}</span>
+                }
+                </div>
+                <div className={style.field}>
+                    <span>{github || 'Jhonny_Doe'}</span>
+                </div>
+                <div className={style.field}>
+                    <span>{twitter || '@Doe44'}</span>
+                </div>
             </div>
             <img src={image} alt="myImage" width="150" height="150"/>
         </div>
