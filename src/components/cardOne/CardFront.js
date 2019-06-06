57a59783ef3bb3b7057356ddce989e509ef93108
R-Layer <layerdev@protonmail.com>
2019-06-06 15:46:33 +0200

    add button to load custom image

--- src/components/cardOne/CardFront.js
+++ src/components/cardOne/CardFront.js
@@ -6,9 +6,17 @@ import image from './imgs/placeholder.jpg';
 const CardFront = props => {
     const {firstName, lastName, email} = props.state;
     return (
-         <div className={cardStyle.test}>
-            <img src={image} alt="myImage" width="150" height="150"/> 
-            <div className={cardStyle.field}>
+         <div className={style.badge}>
+             <button onClick={spin} className={style.btn}>toggle</button>
+             <figure className={style.figure}>
+                <img src={image} alt="myPhoto" width="150" height="150"/> 
+                <button 
+                    className={style.overlayBtn}
+                    onClick={load}
+                >
+                     + 
+                </button>
+             </figure>
                 <span>{firstName || 'Jhon'}</span>
             </div>
             <div className={cardStyle.field}>
